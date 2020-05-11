89c3097920a4fde129b0b88eea383dd811be7946
thainu00 <62654090+thainu00@users.noreply.github.com>
2020-05-11 23:41:08 +0700

    Add files via upload

---
+++ b5.1.py
@@ -0,0 +1,10 @@
+def square(n):
+    return n*n
+def cube(n):
+    return n*n*n
+def avarage(values):
+    nvals=len(values):
+        sum=0.0
+        for v in values:
+            sum +=v
+            return float (sum)/nvals
